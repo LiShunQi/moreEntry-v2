1e0b4e971fbc3d3c9831ec2ccfa601f72d246473
LiShunQi <ggshun7@126.com>
2017-12-11 14:08:43 +0800

    小修改

--- src/views/dzjkxq/slider.js
+++ src/views/dzjkxq/slider.js
@@ -1,12 +1,16 @@
 /**
  * Created by lsq on 2017/12/11.
  */
+require('libs/amazeUI/css/amazeui.min.css');
 require('./slider.css');
 require('libs/amazeUI/js/amazeui.min.js');
-$(function () {
+// $(function ($) {
     $('.am-slider').flexslider({
         animationLoop: false,            // Boolean: 是否循环播放
         directionNav: false,             // Boolean: 是否创建上/下一个按钮（previous/next）
-        slideshow: false,                // Boolean: 是否自动播放
+        slideshow: false               // Boolean: 是否自动播放
     });
-});
+// });
+    $('.dzjkxq-close-icon').click(function () {
+       $('.dzjkxq-wrap').fadeOut();
+    });
