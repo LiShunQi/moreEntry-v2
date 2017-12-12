1e4df09ed93e8eacbf0d67a096b265eabf6a59c4
LiShunQi <ggshun7@126.com>
2017-12-12 09:59:47 +0800

    dzjkxq

--- src/views/dzjkxq/slider.js
+++ src/views/dzjkxq/slider.js
@@ -5,12 +5,16 @@ require('libs/amazeUI/css/amazeui.min.css');
 require('./slider.css');
 require('libs/amazeUI/js/amazeui.min.js');
 // $(function ($) {
-    $('.am-slider').flexslider({
-        animationLoop: false,            // Boolean: 是否循环播放
-        directionNav: false,             // Boolean: 是否创建上/下一个按钮（previous/next）
-        slideshow: false               // Boolean: 是否自动播放
+    $('.test-btn').click(function () {
+        $('.dzjkxq-wrap').fadeIn();
+            $('.am-slider').flexslider({
+                animationLoop: false,            // Boolean: 是否循环播放
+                directionNav: false,             // Boolean: 是否创建上/下一个按钮（previous/next）
+                slideshow: false               // Boolean: 是否自动播放
+            });
+    });
+    $('.dzjkxq-close-icon').click(function () {
+        $('.dzjkxq-wrap').fadeOut();
     });
 // });
-    $('.dzjkxq-close-icon').click(function () {
-       $('.dzjkxq-wrap').fadeOut();
-    });
+
