a9f25c3d0738afeb1974d04f05eb0b44ab52cf46
LiShunQi <ggshun7@126.com>
2017-12-22 18:16:24 +0800

    slide

--- src/views/slide/slide.js
+++ src/views/slide/slide.js
@@ -1,3 +1,38 @@
 /**
  * Created by lsq on 2017/12/22.
  */
+require('static/css/base.css');
+require('static/fonts/iconfont.css');
+require('./slide.scss');
+
+// var reg = /\d{4}(\-|\/|.)\d{1,2}\1\d{1,2}/g;
+// var str = 'jsjjfsj2017.2.1sjfjddddddddfj';
+//
+// console.log(reg.test(str));
+// console.log(reg.lastIndex);
+(function () {
+    let slides = document.querySelectorAll('.gg-slides')[0]
+        ,next = document.querySelectorAll('.slide-next')[0]
+        ,pre = document.querySelectorAll('.slide-pre')[0]
+        ,left = 0
+        ,index = 1;
+        
+        next.addEventListener('click',function () {
+            if(index < 3){
+                index++;
+                left = -(index - 1)*500;
+                slides.style.left = left + 'px';
+            }else{
+                index = 1;
+            }
+        },false);
+
+    pre.addEventListener('click',function () {
+        if(left < 1000){
+            left -= 500;
+        }else{
+            left = 0;
+        }
+        slides.style.left = left + 'px';
+    },false)
+})();
